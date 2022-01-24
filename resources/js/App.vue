0a76494c1608b51ad885f7e93f21e250a699267a
dev-rashed <rashed.eee.brur@gmail.com>
2022-01-25 01:39:29 +0600

    added navbar App.vue

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <router-link to="/products" class="nav-link"
+                        >Products</router-link
+                    >
+                    <router-link to="/products/create" class="nav-link"
+                        >Create</router-link
+                    >
+                </div>
+            </div>
+        </nav>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import routes from "./router";
 
-Vue.use(Vue);
+Vue.use(BootstrapVue);
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
 
